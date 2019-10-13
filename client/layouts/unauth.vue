e36fc655e141d2a65c07dab848551fa536d34982
Ivan Pkh <ivanoid256@inbox.lv>
2019-10-13 15:11:28 +0500

    Login page template.

---
+++ client/layouts/unauth.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="unauth">
+    <a-layout>
+      <a-layout-header>
+        <Header />
+      </a-layout-header>
+      <a-layout-content>
+        <nuxt />
+      </a-layout-content>
+      <a-layout-footer>
+        <Footer />
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.unauth {
+  .ant-layout-content {
+    min-height: 85vh;
+
+    > * {
+      margin: 0 auto;
+      margin-top: 100px;
+    }
+  }
+}
+</style>
--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,9 +1,84 @@
 <template>
-  <div class="content"> <h2> Логин </h2> </div>
+  <div class="content">
+    <h2>Логин</h2>
+    <a-form
+      id="components-form-demo-normal-login"
+      :form="form"
+      class="login-form"
+      @submit="handleSubmit"
+    >
+      <a-form-item>
+        <a-input
+          v-decorator="[
+          'userName',
+          { rules: [{ required: true, message: 'Please input your username!' }] }
+        ]"
+          placeholder="Логин или Е-майл"
+        >
+          <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-decorator="[
+          'password',
+          { rules: [{ required: true, message: 'Please input your Password!' }] }
+        ]"
+          type="password"
+          placeholder="Пароль"
+        >
+          <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-checkbox
+          v-decorator="[
+          'remember',
+          {
+            valuePropName: 'checked',
+            initialValue: false,
+          }
+        ]"
+        >Чужой компьютер</a-checkbox>
+        <a class="login-form-forgot" href>Восстановить пароль</a>
+        <a-button type="primary" html-type="submit" class="login-form-button">Войти</a-button>
+        <nuxt-link to="/signup">Регистрация</nuxt-link>
+      </a-form-item>
+    </a-form>
+  </div>
 </template>
 
 <script>
-  export default {
-    layout: 'dashboard'
+export default {
+  layout: "unauth",
+  beforeCreate() {
+    this.form = this.$form.createForm(this);
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+        }
+      });
+    }
   }
+};
 </script>
+
+<style lang="scss" scoped>
+.content {
+  width: 400px;
+
+  .login-form {
+    max-width: 400px;
+  }
+  .login-form-forgot {
+    float: right;
+  }
+  .login-form-button {
+    width: 100%;
+  }
+}
+</style>
