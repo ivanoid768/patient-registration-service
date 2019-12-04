6783764cf22ba5d37cbe2cea369bc2b759af2b71
Ivan Pkh <ivanoid256@inbox.lv>
2019-12-04 17:39:49 +0500

    Add owner model

--- server/src/modules/auth/user.service.ts
+++ server/src/modules/auth/user.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './signup.dto';
 import { IUser } from 'src/models/users';
+import {Owner} from 'src/models/owner'
 
 export interface IUserService {
   create(arg1: CreateUserDto): Promise<IUser>
@@ -11,7 +12,10 @@ export interface IUserService {
 
 @Injectable()
 export class UserService implements IUserService {
-  constructor(@InjectModel('User') private readonly userModel: Model<IUser>) {}
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<IUser>
+    // @InjectModel(Owner.OwnerToken) private readonly ownerModel: Model<Owner.IOwner>
+  ) {}
 
   async create(createUserDto: CreateUserDto): Promise<IUser> {
     return this.userModel.create(createUserDto);
