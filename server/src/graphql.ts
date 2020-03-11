f7c0bf655f3915c4ad4910fbdc7f005a0beadbcc
Ivan Pkh <ivanoid256@inbox.lv>
2020-03-11 11:26:56 +0500

    Graphql schema updated.

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -39,6 +39,81 @@ export class CreateReceptionistInput {
     password: string;
 }
 
+export class CreateScheduleInput {
+    JanuaryOptId: string;
+    FebruaryOptId?: string;
+    MarchOptId?: string;
+    AprilOptId?: string;
+    MayOptId?: string;
+    JuneOptId?: string;
+    JulyOptId?: string;
+    AugustOptId?: string;
+    SeptemberOptId?: string;
+    OctoberOptId?: string;
+    NovemberOptId?: string;
+    DecemberOptId?: string;
+}
+
+export class DayScheduleInput {
+    from: string;
+    to: string;
+}
+
+export class MonthScheduleInput {
+    firstWeekOptId: string;
+    secondWeekOptId?: string;
+    thirdWeekOptId?: string;
+    fourthWeekOptId?: string;
+}
+
+export class Pause {
+    from: string;
+    to: string;
+}
+
+export class WeekScheduleInput {
+    MondayDayOptId: string;
+    TuesdayDayOptId?: string;
+    WednesdayDayOptId?: string;
+    ThursdayDayOptId?: string;
+    FridayDayOptId?: string;
+    SaturdayDayOptId?: string;
+    SundayDayOptId?: string;
+}
+
+export class AddDayScheduleOptResp {
+    daySchedule: DayScheduleOpt;
+    nextStep: string;
+}
+
+export class AddMonthScheduleOptResp {
+    monthScheduleOpt: MonthScheduleOpt;
+    nextStep: string;
+    prevStep: string;
+}
+
+export class AddWeekScheduleOptResp {
+    weekScheduleOpt: WeekScheduleOpt;
+    nextStep: string;
+    prevStep: string;
+}
+
+export class Appointment {
+    free: boolean;
+    doctor: Doctor;
+    patient: Patient;
+    from: string;
+    to: string;
+    notes: string;
+    timeslot: Timeslot;
+    schedule: Schedule;
+}
+
+export class DayScheduleOpt {
+    id: string;
+    timeslots: TimeRange[];
+}
+
 export class Doctor {
     id: string;
     name: string;
@@ -51,7 +126,31 @@ export class Doctor {
     specialization: string;
 }
 
+export class MonthScheduleOpt {
+    id: string;
+    firstWeek: WeekScheduleOpt;
+    secondWeek: WeekScheduleOpt;
+    thirdWeek: WeekScheduleOpt;
+    fourthWeek: WeekScheduleOpt;
+}
+
 export abstract class IMutation {
+    abstract cancelAppointment(id: string): Appointment | Promise<Appointment>;
+
+    abstract rearrangeAppointment(appointmentId: string, timeslotId: string): Appointment | Promise<Appointment>;
+
+    abstract setAppointmentDuration(duration: number): ScheduleSettings | Promise<ScheduleSettings>;
+
+    abstract addDayScheduleOpt(input: DayScheduleInput, pauses: Pause[]): AddDayScheduleOptResp | Promise<AddDayScheduleOptResp>;
+
+    abstract addWeekScheduleOpt(input: WeekScheduleInput): AddWeekScheduleOptResp | Promise<AddWeekScheduleOptResp>;
+
+    abstract addMonthScheduleOpt(input: MonthScheduleInput): AddMonthScheduleOptResp | Promise<AddMonthScheduleOptResp>;
+
+    abstract createSchedule(input: CreateScheduleInput): Schedule | Promise<Schedule>;
+
+    abstract assignSchedule(scheduleId: string, doctors: Doctor[]): Schedule | Promise<Schedule>;
+
     abstract addDoctor(input: CreateDoctorInput): Doctor | Promise<Doctor>;
 
     abstract removeDoctor(id: string): Doctor | Promise<Doctor>;
@@ -84,7 +183,27 @@ export class Owner {
     confirmed?: boolean;
 }
 
+export class Patient {
+    name: string;
+    surname: string;
+    middlename?: string;
+    phone?: string;
+    email?: string;
+}
+
 export abstract class IQuery {
+    abstract getFreeAppointmentList(doctorId: string): Timeslot[] | Promise<Timeslot[]>;
+
+    abstract scheduleSettings(): ScheduleSettings | Promise<ScheduleSettings>;
+
+    abstract daySheduleList(): DayScheduleOpt[] | Promise<DayScheduleOpt[]>;
+
+    abstract weekSheduleList(): WeekScheduleOpt[] | Promise<WeekScheduleOpt[]>;
+
+    abstract monthSheduleList(): MonthScheduleOpt[] | Promise<MonthScheduleOpt[]>;
+
+    abstract getDoctorSchedule(doctorId: string): Schedule | Promise<Schedule>;
+
     abstract listDoctor(search?: string): Doctor[] | Promise<Doctor[]>;
 
     abstract listOwner(search?: string): Owner[] | Promise<Owner[]>;
@@ -105,6 +224,32 @@ export class Receptionist {
     confirmed?: boolean;
 }
 
+export class Schedule {
+    id: string;
+    timeslots: Timeslot[];
+    doctors: Doctor[];
+    monthScheduleOptList: MonthScheduleOpt[];
+    creator: User;
+    createdAt: string;
+}
+
+export class ScheduleSettings {
+    appointmentDuration: number;
+}
+
+export class TimeRange {
+    from: string;
+    to: string;
+}
+
+export class Timeslot {
+    from: string;
+    to: string;
+    doctors: Doctor[];
+    schedule: Schedule;
+    appointment?: Appointment;
+}
+
 export class User {
     id: string;
     name: string;
@@ -113,4 +258,15 @@ export class User {
     email?: string;
     phone?: string;
     role: Role;
+}
+
+export class WeekScheduleOpt {
+    id: string;
+    Monday: DayScheduleOpt;
+    Tuesday: DayScheduleOpt;
+    Wednesday: DayScheduleOpt;
+    Thursday: DayScheduleOpt;
+    Friday: DayScheduleOpt;
+    Saturday: DayScheduleOpt;
+    Sunday: DayScheduleOpt;
 }
